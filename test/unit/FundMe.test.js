7e83f527e2937ebf7eba94b85051ec57d3e7eab9
AFPedreros <felipe.pedreros94@gmail.com>
2022-09-19 17:40:46 -0500

    testing constructor

---
+++ test/unit/FundMe.test.js
@@ -0,0 +1,24 @@
+const { assert } = require("chai")
+const { deployments, ethers, getNamedAccounts } = require("hardhat")
+
+describe("FundMe", () => {
+    let fundMe
+    let deployer
+    let mockV3Aggregator
+    beforeEach(async () => {
+        deployer = (await getNamedAccounts()).deployer
+        await deployments.fixture(["all"])
+        fundMe = await ethers.getContract("FundMe", deployer)
+        mockV3Aggregator = await ethers.getContract(
+            "MockV3Aggregator",
+            deployer
+        )
+    })
+
+    describe("constructor", async () => {
+        it("set the aggregator addresses correctly", async () => {
+            const response = await fundMe.priceFeed()
+            assert.equal(response, mockV3Aggregator.address)
+        })
+    })
+})
